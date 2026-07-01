58facfe8b4947913c603608e335e78b012b864d2
Melat Endalamaw <m34694884@gmail.com>
2026-07-01 17:06:45 +0300

    Initial commit- Task Manager Project

---
+++ todo_list.py
@@ -0,0 +1,52 @@
+print("=" * 30)
+print("  Welcome To Your Task Manager")
+print("=" * 30)
+def task_manager():
+    tasks = []
+    while (True):
+        print("Menu")
+        print("1. Add Task")
+        print("2. View Task")
+        print("3. Delete Task")
+        print("4. Exit")
+
+        try:
+            choose = int(input("choose: "))
+        except ValueError:
+            print("Please enter a number!")
+            continue
+
+        if (choose == 1):
+            print("enter task")
+            task = input("task: ")
+            tasks.append(task)
+        elif (choose == 2):
+            if tasks:
+                print("==Todo LIst==")
+                for i in range(len(tasks)):
+                    print(f"{i+1}. {tasks[i]}")
+            else:
+                print("The list empty")
+        elif (choose == 3):
+            if tasks:
+                print("which task do u want to remove from the list")
+                for i in range(len(tasks)):
+                    print(f"{i+1}. {tasks[i]}")
+                try:
+                    j = int(input("Task Number: "))
+                except ValueError:
+                    print("Invalid input. Enter a valid number.")
+                    continue
+                # if tasks[j]:
+                if j >= 1 and j <= len(tasks):
+                    removed_task= tasks.pop(j-1)
+                    print(f"Task = {removed_task} is removed")
+                else:
+                    print("not exist")
+        elif (choose == 4):
+            break
+        else:
+            print("invalid choose")
+
+
+task_manager()
